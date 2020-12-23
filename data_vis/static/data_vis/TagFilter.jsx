836257403054242fe2971fb3e9c0dfd909b2d199
Jim Neuendorf <jim.neuendorf@gmx.de>
2020-12-24 00:46:25 +0100

    using antd for tag filters

--- data_vis/static/data_vis/TagFilter.jsx
+++ data_vis/static/data_vis/TagFilter.jsx
@@ -1,46 +1,61 @@
-import React from 'react'
-import { Spin } from 'antd'
+import React, { useCallback } from 'react'
+import {
+    Spin,
+    Form,
+    Checkbox,
+} from 'antd'
+import styled from 'styled-components'
+
+
+const FormWrapper = styled.div`min-height: 100px;`
 
 
 export const TagFilter = ({tags, highlightedTags, setHighlightedTags}) => {
+    const toggleCheckbox = useCallback(
+        event => {
+            const {
+                target: {
+                    checked,
+                    "data-tag": tag,
+                },
+            } = event
+            if (checked) {
+                setHighlightedTags([...highlightedTags, tag])
+            }
+            else {
+                setHighlightedTags(highlightedTags.filter(
+                    ignoredTag => ignoredTag !== tag
+                ))
+            }
+        },
+        [highlightedTags, setHighlightedTags],
+    )
+
     return <Spin
         spinning={tags.length === 0}
         tip='Loading...'
         delay={100}
     >
-        <ul style={{ paddingInlineStart: '15px', minHeight: '120px' }}>
-            {
-                tags
-                .filter(({is_abstract}) => !is_abstract)
-                .map(({identifier: tag}) => {
-                    const checked = highlightedTags.indexOf(tag) >= 0
-                    const id = `checkbox_${tag}`
-                    return <li
-                        key={tag}
-                        style={{ listStyleType: 'none' }}
-                    >
-                        <input
-                            id={id}
-                            type='checkbox'
-                            checked={checked}
-                            onChange={event => {
-                                if (checked) {
-                                    setHighlightedTags(highlightedTags.filter(
-                                        ignoredTag => ignoredTag !== tag
-                                    ))
-                                }
-                                else {
-                                    setHighlightedTags([...highlightedTags, tag])
-                                }
-                            }}
-                        />
-                        <label htmlFor={id}>
-                            {tag}
-                        </label>
-                    </li>
-                })
-            }
-        </ul>
+        <FormWrapper>
+            <Form layout='vertical' size='small'>
+                {
+                    tags
+                    .filter(({is_abstract}) => !is_abstract)
+                    .map(({identifier: tag}) => {
+                        const checked = highlightedTags.indexOf(tag) >= 0
+                        return <Form.Item key={tag}>
+                            <Checkbox
+                                data-tag={tag}
+                                checked={checked}
+                                onChange={toggleCheckbox}
+                            >
+                                {tag}
+                            </Checkbox>
+                        </Form.Item>
+                    })
+                }
+            </Form>
+        </FormWrapper>
     </Spin>
 
 }
